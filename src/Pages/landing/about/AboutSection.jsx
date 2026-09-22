8f88a51d7ee68ec5701ff722a0ce378f29a31479
WdataW <wael272726@gmail.com>
2026-09-22 04:22:59 +0300

    edit LandingPage about-section gap

--- src/Pages/landing/about/AboutSection.jsx
+++ src/Pages/landing/about/AboutSection.jsx
@@ -6,7 +6,7 @@ const innerSections = [
 export default function AboutSection({ className = "", children, ...props }) {
     return (
         <div id="about" className={`${paddingX} border-b py-[5rem] md:py-[8rem] ${className}`} {...props}>
-            <ul className="flex flex-col gap-[8rem]">
+            <ul className="flex flex-col  lg:gap-[7rem] md:gap-[6rem] gap-[4rem] ">
                 {innerSections.map((key, i) => <li key={i}><AboutInnerSection invert={i % 2 == 1} titleKey={key} /></li>)}
             </ul>
         </div>
